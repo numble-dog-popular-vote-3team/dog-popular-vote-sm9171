d363aa61d7495efe5a23b898f1d476a42e9a5d2b
Sangmin <sm9171@nate.com>
2023-05-23 15:24:02 +0900

    feat: kafka 샘플코드

---
+++ src/main/java/com/numble/dogpopularvote/dog/api/controller/SampleController.java
@@ -0,0 +1,25 @@
+package com.numble.dogpopularvote.dog.api.controller;
+
+import com.numble.dogpopularvote.dog.api.service.KafkaProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@CrossOrigin("*")
+@RestController
+@RequestMapping(value = "/kafka/test")
+public class SampleController {
+	private final KafkaProducer producer;
+
+	@Autowired
+	SampleController(KafkaProducer producer) {
+		this.producer = producer;
+	}
+
+	@PostMapping(value = "/message")
+	public String sendMessage(@RequestParam("message") String message) {
+		this.producer.sendMessage(message);
+		return "success";
+	}
+}
---
+++ src/main/java/com/numble/dogpopularvote/dog/api/service/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.numble.dogpopularvote.dog.api.service;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class KafkaConsumer {
+
+	@KafkaListener(topics = "${message.topic.name}", groupId = ConsumerConfig.GROUP_ID_CONFIG)
+	public void consume(String message) throws IOException {
+		System.out.println(String.format("Consumed message : %s", message));
+	}
+}
