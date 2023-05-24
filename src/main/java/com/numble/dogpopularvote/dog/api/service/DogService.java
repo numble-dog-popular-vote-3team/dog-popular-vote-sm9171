5cdf0893503d8e06f7f584280819a575acd08272
Sangmin <sm9171@nate.com>
2023-05-25 00:45:38 +0900

    feature : 강아지 상세 조회 기능 구현

--- src/main/java/com/numble/dogpopularvote/dog/api/service/DogService.java
+++ src/main/java/com/numble/dogpopularvote/dog/api/service/DogService.java
@@ -1,7 +1,10 @@
 package com.numble.dogpopularvote.dog.api.service;
 
+import com.numble.dogpopularvote.dog.api.dto.DogResponse;
 import com.numble.dogpopularvote.dog.api.dto.DogsResponse;
+import com.numble.dogpopularvote.dog.domain.entity.Dog;
 import com.numble.dogpopularvote.dog.domain.repository.DogRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,5 +19,10 @@ public class DogService {
 
     public Page<DogsResponse> getDogs(PageRequest pageRequest) {
         return DogsResponse.from(dogRepository.findAll(pageRequest));
+    }
+
+    public DogResponse getDog(String dogId) {
+        Dog findedDog = dogRepository.findById(Long.valueOf(dogId)).orElseThrow(() -> new EntityNotFoundException());
+        return DogResponse.from(findedDog);
     }
 }
